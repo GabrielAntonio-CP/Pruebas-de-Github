36dceb26c71e31ed0b4a8e931d351a2973efc91e
GabrielAntonio-CP <a376997@uach.mx>
2026-05-02 19:05:53 -0600

    primeras lecciones de 'apendiendo python con MoureDev'

---
+++ 00_hello.py
@@ -0,0 +1,22 @@
+# esto es un comentario
+print("Hola python") 
+
+"""
+esto es un comentario entre vairas lineas
+es decir que puedo escribir lo que sea
+y no va a afectar al codigo
+"""
+
+
+# Consultar el tipo de dato
+print(type('Hola')) # esto sirve para saver el tipo de dato de lo que se esta imprimendo
+
+print(type("Soy un dato str")) # Tipo 'str'
+
+print(type(5)) # Tipo 'int'
+
+print(type(1.5)) # Tipo 'float'
+
+print(type(3 + 1j)) # Tipo 'complex'
+
+print(type(True)) # Tipo 'bool'
---
+++ 01_variables.py
@@ -0,0 +1,13 @@
+#Variables
+
+#un buen uso de las variables seria nombrarlas en minuscula y con guion para separar las palabras 
+my_variable =  "My string variable"
+print(my_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_bool_variable = False
+print (my_bool_variable)
+
+print(my_bool_variable, my_variable)
