acf249c19e95cafaebac9a5a17e1e214c32dc343
GabrielAntonio-CP <a376997@uach.mx>
2026-05-04 20:02:39 -0600

    Fin del capitulo de variables - Mon 4 May 2026

--- 01_variables.py
+++ 01_variables.py
@@ -10,4 +10,20 @@ print(my_int_variable)
 my_bool_variable = False
 print (my_bool_variable)
 
+#concatencacion de variables
 print(my_bool_variable, my_variable)
+
+print(len(my_variable)) # esto sirve para saber la cantidad de caracteres que tiene una variable
+
+nombre, apellido, edad, ciudad = "Juan", "Perez", 23, "Mexico"
+# esto es una forma de asignar varias variables en una sola linea.
+
+print(nombre, apellido, edad, ciudad)
+
+name = "Juana"
+print(name)
+print (type(name))
+
+name = 5
+print(name)
+print (type(name))
