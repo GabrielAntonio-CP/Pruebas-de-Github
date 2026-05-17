76c6eef6090fe99e19c8bbd304489ad3147a6131
GabrielAntonio-CP <a376997@uach.mx>
2026-05-17 16:20:58 -0600

    Aprendiendo python con MoureDev, clases - 17 May 2026

---
+++ 11_clases.py
@@ -0,0 +1,30 @@
+### Clases ###
+
+class MyEmptyPerson: #una buena practica es utilizar camel case para definir el nombre de una clase
+    pass
+
+class Person:
+    def __init__(self, name, surname): #el "init" es una palabra reservada de python que sirve para iniciar un constructor
+        #hay que almacenar los datos utilizando self(nombredeparametro) = variable con el parametro
+        #esto crea las propiedades del objeto
+        self.name = name
+        self.surname = surname
+
+class Person2:
+    def __init__(self, name, surname): 
+        self.fullname = f"{name} {surname}"
+
+    def walk (self): #se debe se pasar self siempre que se quieran usar parametros de la misma clase
+        print(f"{self.fullname} Esta caminando")
+       
+
+my_person = Person("juan","perez")
+print(f"{my_person.name} {my_person.surname}") #podemos crear propiedades con la concatenacion de nuestros parametros recibidos
+
+my_person2 = Person2("Luis", "Valentino")
+print(my_person2.fullname)
+my_person2.walk()
+
+my_person2.fullname = "Alice M. la CEO"
+print(my_person2.fullname)
+my_person2.walk()
